6dc3674100cccaf385eb48678bf3816895128aed
simonestaffa <simone.staffa@tutored.it>
2020-02-24 23:07:54 +0100

    Initialiazed project Auth APIs

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,22 @@
 import {createConnection} from "typeorm";
 
 export let connect = async () => {
-    const connection = await createConnection();
+    const connection = await createConnection({
+        "type": "sqlite",
+        "database": "database.sqlite",
+        "name": "default",
+        "synchronize": false,
+        "logging": true,
+        "migrationsTableName": "custom_migration_table",
+        "entities": [
+            "src/db/models/**/*.ts"
+        ],
+        "migrations": [
+            "src/db/migration/*.ts"
+        ],
+        "cli": {
+            "entitiesDir": "src/db/models",
+            "migrationsDir": "src/db/migration"
+        }
+    });
 };
