3caf0cdca868ef581bef8bebae2d5f5d8e47a380
FrancescoAmorosini <francesco.amorosini@stud.unifi.it>
2020-02-27 20:00:30 +0100

    added get user by id

---
+++ src/api/users/getbyid/GetById.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import {RequestController} from "../../../routing-utilities/RequestController";
+import {Request, Response, NextFunction} from "express";
+import * as Joi from "joi";
+import {User} from "../../../db/models/User.model";
+import {XError} from "../../../routing-utilities/XError";
+
+export class GetById extends RequestController {
+    validate?: Joi.JoiObject;
+
+    async getById(req: Request): Promise<User> {
+        // Each user can only retrieve its own profile
+        if((req.user.id).toString() === req.params.id){
+            const id = req.user.id
+            const user = await User.findOne({
+                    where: {
+                        id,
+                        role: 'user' }
+            });
+            if(!user)
+                throw new XError(User.INVALID_CREDENTIALS_ERROR, 403, "Invalid credentials");
+            else
+                return user;
+
+        }
+        else{
+            throw new XError(User.INVALID_CREDENTIALS_ERROR, 403, "Invalid credentials");
+        }
+    };
+
+    async exec(req: Request, res: Response, next: NextFunction): Promise<User> {
+        return await this.getById(req);
+    }
+
+}
--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -2,11 +2,13 @@ import {AbstractRoutes, MethodRoutes} from "../../routing-utilities/AbstractRout
 import {Route} from "../../routing-utilities/Route";
 import {PathRoute} from "../../routing-utilities/PathRoute";
 import { GetAll } from "./getall/GetAll";
+import { GetById } from "./getbyid/GetById";
 
 export class UserRoutes extends AbstractRoutes {
     initializeRoutes(): Route[] {
         return [
-            new Route(MethodRoutes.GET, '/', {'v1': new GetAll()}, "user")
+            new Route(MethodRoutes.GET, '/', {'v1': new GetAll()}, "user"),
+            new Route(MethodRoutes.GET, '/:id', {'v1': new GetById()}, "user")
         ];
     }
 
